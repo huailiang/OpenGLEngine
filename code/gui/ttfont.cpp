5d4a7e38667ca71c5ae2a9b7141b0fea1770698d
Huailiang Peng <penghuailiang@126.com>
2019-06-03 14:42:13 +0800

    资源聚合

--- code/gui/ttfont.cpp
+++ code/gui/ttfont.cpp
@@ -71,7 +71,7 @@ int TTFont::initial()
     if (FT_Init_FreeType(&ft)) std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
     
     FT_Face face;
-    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face)) std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    if (FT_New_Face(ft, "resources/fonts/arial.ttf", 0, &face)) std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
     
     FT_Set_Pixel_Sizes(face, 0, FONT_SIZE);
     
